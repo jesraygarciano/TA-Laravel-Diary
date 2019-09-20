dbb4635741865cac7f56660ca1a9a816b08c42c3
Jesray Garciano <jesray.garciano@gmail.com>
2019-09-20 13:04:53 +0800

    Dependencies updated

--- resources/views/diaries/index.blade.php
+++ resources/views/diaries/index.blade.php
@@ -8,6 +8,7 @@
 <a href="{{ route('diary.create') }}" class="btn btn-primary btn-block">
     新規投稿
 </a>
+
 @foreach ($diaries as $diary)
     <div class="mt-4 p-3 border border-primary">
         @if($diary->image_path !== null)
@@ -24,6 +25,9 @@
                 <button class="btn btn-danger">削除</button>
             </form>
         @endif
+        <form action="" method="POST">
+            <button disabled="disabled">Submit</button>
+        </form>
         <div class=" mt-3 ml-3">
             @if (Auth::check() && $diary->likes->contains(function ($user) {
                 return $user->id === Auth::user()->id;
@@ -34,6 +38,7 @@
             @endif
             <input class="diary-id" type="hidden" value="{{ $diary->id }}">
             <span class="js-like-num">{{ $diary->likes->count() }}</span>
+            
         </div>
     </div>
 @endforeach
--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::group(['middleware' => 'auth'], function() {
     Route::get('diary/{diary}/edit', 'DiaryController@edit')->name('diary.edit');
     Route::put('diary/{diary}/update', 'DiaryController@update')->name('diary.update');
 
-    Route::delete('diary/{diary}/delete', 'DiaryController@destroy')->name('diary.destroy');    
+    Route::delete('diary/{id}/delete', 'DiaryController@destroy')->name('diary.destroy');    
 
     Route::post('diary/{id}/like', 'DiaryController@like');
     Route::post('diary/{id}/dislike', 'DiaryController@dislike');
